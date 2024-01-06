2986f084838d58cb6ac6df63b63681fb6717980c
eloitr <eloitraver@gmail.com>
2024-01-06 23:28:19 +0100

    un poco de sintaxis

--- SINTAXIS/03strings.py
+++ SINTAXIS/03strings.py
@@ -27,3 +27,38 @@ print("Em dic {} {} i tinc {} anys.".format(name, surname, age)) #en format {}
 print("Em dic %s %s i tinc %s anys." %(name, surname, age)) # per % posar al final % i variables
 #mejor que:
 print("Em dic " + name + " " + surname + " i tinc " + str(age) + " anys." )
+print(f"Mi nombre es {name} {surname} i tinc {age} anys.")
+
+#DESEMPAQUETADO DE CARACTERES
+language = "python"
+a, b, c, d, e, f = language #cada letra
+print(a)
+print(c)
+
+#DIVISION
+language_slice = language[1:3]#de la 1 a la 3 sin contar la 3
+print(language_slice)
+
+language_slice = language[1:]#todo menos la primera letra
+print(language_slice)
+
+language_slice = language[-2]#la segunda empezando por el final
+print(language_slice)
+
+language_slice = language[0:6:2]#rimero pilla un rango y despues dice otro rango8ver ejemplo
+print(language_slice)
+
+#REVERSE
+reversed_language = language[::-1]#dar la vuelta al str
+print(reversed_language)
+
+
+#MÉTODOS/FUNCIONES
+print(language.capitalize())#posa la primera en majuscula
+print(language.upper())#todo mayus
+print(language.count("t"))#cuenta cuántas "t" tiene
+print(language.isnumeric())#nos dice si es un número(bool)
+print(language.lower())#todas minúsculas
+print(language.upper().isupper())#nos dice si es upper o no
+print(language.lower().isupper())
+print(language.startswith("py"))#nos dice si empieza por...
--- SINTAXIS/04listas.py
+++ SINTAXIS/04listas.py
@@ -5,8 +5,8 @@ my_other_list = []
 
 print(len(my_list))
 
-my_list = [15, 56, 34, 20, 20]
-my_list = list([15, 56, 34, 20, 20])
+my_list = [15, 56, 33, 11, 22, 44, 55, 66, 34, 20, 20]
+my_list = list([15, 56, 33, 11, 22, 44, 55, 66, 34, 20, 20])
 print (my_list)
 print (len(my_list)) #numero de datos de la lista
 
@@ -28,8 +28,56 @@ print(name)
 
 print (my_list + my_other_list) #s'ajunten
 
+#AÑADIR O CAMBIAR EN UNA LISTA
 my_other_list.append("Bon dia")
 print(my_other_list)
 
 my_other_list.insert(0, "blue")
 print(my_other_list)
+
+my_other_list[0] = "Rojo" #Para cambiar alores
+print(my_other_list)
+
+
+
+
+#BORRADORES
+my_other_list.remove("Rojo")
+print(my_other_list)
+
+my_list.remove(20)
+print(my_list)#només treu un
+
+my_list.pop()#elimina el ultio elemento (por defecto)
+print(my_list)
+
+print(my_list.pop()) # retorna el que ha eliminat
+print(my_list)
+
+my_list.pop(2)#elimina el segon(tercer realmet:0, 1, 2) element
+print(my_list) 
+#també ho pots guardar en una variable
+my_pop_element = my_list.pop(2)
+
+del my_list[2]
+print(my_list)
+
+my_new_list = my_list.copy() #para copiar
+
+my_list.clear() #eliminar todos los elemetos de la lista
+print(my_list)
+print(my_new_list)
+
+my_new_list.reverse()#cambia el orden, da la vuelta
+print(my_new_list.reverse())
+
+my_new_list.sort()#orden alfabético, de numeros
+print(my_new_list)
+
+
+print(my_new_list[1:3])#posa 2 xifres, el 1 i el 2
+
+
+
+
+
