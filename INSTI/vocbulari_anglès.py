c65757db8f7fd2d6fd7295e8e049ee4224150c9b
eloitr <eloitraver@gmail.com>
2024-03-08 13:02:55 +0100

    estudi

--- INSTI/vocbulari_anglès.py
+++ INSTI/vocbulari_anglès.py
@@ -1,23 +1,20 @@
 import random
- 
+
 while True:
-    vocabulari = {"zip": "cremallera", "brand":"marca", "clothing":"vestimenta", "compliment":"cumplido", "deliver":"entregar", "frames":"marcs", "glamorous":"glamurós", 
-                "hoodie":"dessuadora amb caputxa", "sleeve":"màniga", "sleeveless":"sense màniga", "stylish":"elegant", "suit":"quedar bé", "swap":"intercanviar", "layer":"capes", "plain":"pla" or "llis", "price tag":"etiqueta",
-                  "range":"rango", "ripped":"estripat", "assess":"evaluar", "cheat":"fer trampes", "daydream":"estar als núvols", "draw a conclusion":"arribar a la conclusió", "figure out":"desxifrar" or "resoldre", "focus":"concentrar-se en", "gamble":"apostar",
-                  "make an effort":"fer un esforç", "read someone's mind":"llegir la ment a algú", "recall":"recordar", "recognise":"recordar", "remind":"recordar a algú", "suspect":"sospitar", "take turns":"fer torns", "think over":"reflexionar", "":"",
-                  "airline":"aerolínea", "board":"embarcar", "check in":"facturar", "crew":"tripulació", "delay":"retard", "fabulous":"fabulós", "flight attendant":"assafata" or "assistenta de vol", "full board":"pensió completa", "gate":"porta",
-                  "half board":"mitja pensió", "high season":"temporada alta", "in advance":"per avançat", "lively":"animat", "luxurious":"de luxe", "on a budget":"pressupost", "overcrowded":"molt concorregut" or "molta get", "rate":"nota" or "Classificació", "unforgettable":"inolvidable",
-                  "within walking distance":"a prop" or "a poca distància", "youth hostel":"hostal per a joves" or "hostal jove", "book":"reservar", "take off":"enlairar-se", "land":"aterrrar", "by car":"amb cotxe" or "en cotxe", "on foot":"a peu", "on time":"a temps"
+    vocabulari = {"appearance": "aspecte", "beat":"ritme", "colorful":"colorit", "experience":"experiència", "flavour":"sabor" or "gust", "gorgeous":"preciós" or "bonic", "pattern":"estampat", 
+                "rotten":"podrit", "rough":"rugós", "scent":"olor" or "aroma", "scratch":"rascar" or "acariciar", "shape":"forma", "shiny":"brillant", "smooth":"llis" or "tranquil", "soft":"suau", "stare":"mirar fixament",
+                "stink":"fer pudor" or "apestar", "tickle":"fer pessigolles", "whisper":"xiuxiuejar", "yell":"cridar"
     }
 
     escollir= random.choice(list(vocabulari.items()))
+
     print(escollir[0])
     resposta = input("Significat?:")
     if resposta == escollir[1]:
         print("Molt bé")
     else:
         print("malament")
-  
+
     
 
 
--- PROYECTOS/bot_1.py
+++ PROYECTOS/bot_1.py
@@ -5,28 +5,42 @@ from selenium.webdriver.common.by import By
 import time
 from selenium.webdriver.common.keys import Keys
 from time import sleep
-
-
+##import tkinter as tk
+##from tkinter import ttk 
+"""
+ventana_principal = tk.Tk()
+ventana_principal.config(width=1000, height=500)
+ventana_principal.title("BOT")
+"""
 driver = webdriver.Chrome()
+while True:
 
-def login (driver):
-    driver.get('https://www.instagram.com/accounts/login/')
-    sleep(1)
-    login_button = driver.find_element(By.NAME, "username")
-    login_button.clear()
-    login_button.send_keys("eloitraver2@gmail.com")
-    login_button.clear()
-    sleep(0.3)
-    login_button = driver.find_element(By.NAME, "password")
-    login_button.clear()
-    login_button.send_keys("123456789Hola")
-    login_button.clear()
-    sleep(0.3)
-    login_button.send_keys(Keys.RETURN)
-    login_button.clear()
-    sleep(20)
     
+    def login (driver):
+    
+        driver.get('https://www.instagram.com/accounts/login/')
+        sleep(1)
+        login_button = driver.find_element(By.NAME, "username")
+        login_button.clear()
+        login_button.send_keys("eloitraver2@gmail.com")
+        login_button.clear()
+        sleep(0.3)
+        login_button = driver.find_element(By.NAME, "password")
+        login_button.clear()
+        login_button.send_keys("123456789Hola")
+        login_button.clear()
+        sleep(0.3)
+        login_button.send_keys(Keys.RETURN)
+        login_button.clear()
+        sleep(20)
+"""   
+boto_LOGIN = ttk.Button(
+    ventana_principal,
+    text="Calcular",
+    command=login
+    )
+boto_LOGIN.place(x=400, y=100)
+"""
 
-login (driver)
 
 
